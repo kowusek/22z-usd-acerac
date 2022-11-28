c7cca8a53c96a9bdda804a77d34b284ca53c5dc0
Kuba Grzechociński <jgrzechocinskiwebsensa@grupawp.pl>
2022-11-28 19:22:51 +0100

    change algo template to DQM

--- algos/acer.py
+++ algos/acer.py
@@ -6,7 +6,7 @@ import torch as th
 from gym import spaces
 from torch.nn import functional as F
 
-from stable_baselines3.common.on_policy_algorithm import OnPolicyAlgorithm
+from stable_baselines3.common.off_policy_algorithm import OffPolicyAlgorithm
 from stable_baselines3.common.policies import ActorCriticCnnPolicy, ActorCriticPolicy, BasePolicy, MultiInputActorCriticPolicy
 from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback, Schedule
 from stable_baselines3.common.utils import explained_variance, get_schedule_fn
@@ -14,9 +14,9 @@ from stable_baselines3.common.utils import explained_variance, get_schedule_fn
 ACERSelf = TypeVar("ACERSelf", bound="ACER")
 
 
-class ACER(OnPolicyAlgorithm):
+class ACER(OffPolicyAlgorithm):
     """
-    Actor Critic with Experience Replay
+    The ACER (Actor-Critic with Experience Replay) model class, https://arxiv.org/abs/1611.01224
 
     !TODO: code copied from PPO. The params prepended with `--` are not yet tested and may not be needed
 
@@ -28,7 +28,7 @@ class ACER(OnPolicyAlgorithm):
         (i.e. rollout buffer size is n_steps * n_envs where n_envs is number of environment copies running in parallel)
         NOTE: n_steps * n_envs must be greater than 1 (because of the advantage normalization)
         See https://github.com/pytorch/pytorch/issues/29372
-    --:param batch_size: Minibatch size
+    :param batch_size: buffer replay / minibatch size
     --:param n_epochs: Number of epoch when optimizing the surrogate loss
     --:param gamma: Discount factor
     --:param gae_lambda: Factor for trade-off of bias vs variance for Generalized Advantage Estimator
@@ -174,130 +174,54 @@ class ACER(OnPolicyAlgorithm):
 
     def train(self) -> None:
         """
-        Update policy using the currently gathered rollout buffer.
+        Update policy using the currently gathered replay buffer.
         """
 
-        # !TODO: this is still a PPO implementation, not ACER 
+        # !TODO: this is still a DQM implementation, not ACER
 
         # Switch to train mode (this affects batch norm / dropout)
         self.policy.set_training_mode(True)
-        # Update optimizer learning rate
+        # Update learning rate according to schedule
         self._update_learning_rate(self.policy.optimizer)
-        # Compute current clip range
-        clip_range = self.clip_range(self._current_progress_remaining)
-        # Optional: clip range for the value function
-        if self.clip_range_vf is not None:
-            clip_range_vf = self.clip_range_vf(self._current_progress_remaining)
 
-        entropy_losses = []
-        pg_losses, value_losses = [], []
-        clip_fractions = []
+        losses = []
+        # for _ in range(gradient_steps): <-- from DQN
+        # Sample replay buffer
+        replay_data = self.replay_buffer.sample(self.batch_size, env=self._vec_normalize_env)
 
-        continue_training = True
+        with th.no_grad():
+            # Compute the next Q-values using the target network
+            next_q_values = self.q_net_target(replay_data.next_observations)
+            # Follow greedy policy: use the one with the highest value
+            next_q_values, _ = next_q_values.max(dim=1)
+            # Avoid potential broadcast issue
+            next_q_values = next_q_values.reshape(-1, 1)
+            # 1-step TD target
+            target_q_values = replay_data.rewards + (1 - replay_data.dones) * self.gamma * next_q_values
 
-        # train for n_epochs epochs
-        for epoch in range(self.n_epochs):
-            approx_kl_divs = []
-            # Do a complete pass on the rollout buffer
-            for rollout_data in self.rollout_buffer.get(self.batch_size):
-                actions = rollout_data.actions
-                if isinstance(self.action_space, spaces.Discrete):
-                    # Convert discrete action from float to long
-                    actions = rollout_data.actions.long().flatten()
+        # Get current Q-values estimates
+        current_q_values = self.q_net(replay_data.observations)
 
-                # Re-sample the noise matrix because the log_std has changed
-                if self.use_sde:
-                    self.policy.reset_noise(self.batch_size)
+        # Retrieve the q-values for the actions from the replay buffer
+        current_q_values = th.gather(current_q_values, dim=1, index=replay_data.actions.long())
 
-                values, log_prob, entropy = self.policy.evaluate_actions(rollout_data.observations, actions)
-                values = values.flatten()
-                # Normalize advantage
-                advantages = rollout_data.advantages
-                # Normalization does not make sense if mini batchsize == 1, see GH issue #325
-                if self.normalize_advantage and len(advantages) > 1:
-                    advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)
+        # Compute Huber loss (less sensitive to outliers)
+        loss = F.smooth_l1_loss(current_q_values, target_q_values)
+        losses.append(loss.item())
 
-                # ratio between old and new policy, should be one at the first iteration
-                ratio = th.exp(log_prob - rollout_data.old_log_prob)
+        # Optimize the policy
+        self.policy.optimizer.zero_grad()
+        loss.backward()
+        # Clip gradient norm
+        th.nn.utils.clip_grad_norm_(self.policy.parameters(), self.max_grad_norm)
+        self.policy.optimizer.step()
 
-                # clipped surrogate loss
-                policy_loss_1 = advantages * ratio
-                policy_loss_2 = advantages * th.clamp(ratio, 1 - clip_range, 1 + clip_range)
-                policy_loss = -th.min(policy_loss_1, policy_loss_2).mean()
-
-                # Logging
-                pg_losses.append(policy_loss.item())
-                clip_fraction = th.mean((th.abs(ratio - 1) > clip_range).float()).item()
-                clip_fractions.append(clip_fraction)
-
-                if self.clip_range_vf is None:
-                    # No clipping
-                    values_pred = values
-                else:
-                    # Clip the difference between old and new value
-                    # NOTE: this depends on the reward scaling
-                    values_pred = rollout_data.old_values + th.clamp(
-                        values - rollout_data.old_values, -clip_range_vf, clip_range_vf
-                    )
-                # Value loss using the TD(gae_lambda) target
-                value_loss = F.mse_loss(rollout_data.returns, values_pred)
-                value_losses.append(value_loss.item())
-
-                # Entropy loss favor exploration
-                if entropy is None:
-                    # Approximate entropy when no analytical form
-                    entropy_loss = -th.mean(-log_prob)
-                else:
-                    entropy_loss = -th.mean(entropy)
-
-                entropy_losses.append(entropy_loss.item())
-
-                loss = policy_loss + self.ent_coef * entropy_loss + self.vf_coef * value_loss
-
-                # Calculate approximate form of reverse KL Divergence for early stopping
-                # see issue #417: https://github.com/DLR-RM/stable-baselines3/issues/417
-                # and discussion in PR #419: https://github.com/DLR-RM/stable-baselines3/pull/419
-                # and Schulman blog: http://joschu.net/blog/kl-approx.html
-                with th.no_grad():
-                    log_ratio = log_prob - rollout_data.old_log_prob
-                    approx_kl_div = th.mean((th.exp(log_ratio) - 1) - log_ratio).cpu().numpy()
-                    approx_kl_divs.append(approx_kl_div)
-
-                if self.target_kl is not None and approx_kl_div > 1.5 * self.target_kl:
-                    continue_training = False
-                    if self.verbose >= 1:
-                        print(f"Early stopping at step {epoch} due to reaching max kl: {approx_kl_div:.2f}")
-                    break
-
-                # Optimization step
-                self.policy.optimizer.zero_grad()
-                loss.backward()
-                # Clip grad norm
-                th.nn.utils.clip_grad_norm_(self.policy.parameters(), self.max_grad_norm)
-                self.policy.optimizer.step()
-
-            if not continue_training:
-                break
-
-        self._n_updates += self.n_epochs
-        explained_var = explained_variance(self.rollout_buffer.values.flatten(), self.rollout_buffer.returns.flatten())
-
-        # Logs
-        self.logger.record("train/entropy_loss", np.mean(entropy_losses))
-        self.logger.record("train/policy_gradient_loss", np.mean(pg_losses))
-        self.logger.record("train/value_loss", np.mean(value_losses))
-        self.logger.record("train/approx_kl", np.mean(approx_kl_divs))
-        self.logger.record("train/clip_fraction", np.mean(clip_fractions))
-        self.logger.record("train/loss", loss.item())
-        self.logger.record("train/explained_variance", explained_var)
-        if hasattr(self.policy, "log_std"):
-            self.logger.record("train/std", th.exp(self.policy.log_std).mean().item())
+        # Increase update counter
+        self._n_updates += gradient_steps
 
         self.logger.record("train/n_updates", self._n_updates, exclude="tensorboard")
-        self.logger.record("train/clip_range", clip_range)
-        if self.clip_range_vf is not None:
-            self.logger.record("train/clip_range_vf", clip_range_vf)
-
+        self.logger.record("train/loss", np.mean(losses))
+        
     def learn(
         self: ACERSelf,
         total_timesteps: int,
